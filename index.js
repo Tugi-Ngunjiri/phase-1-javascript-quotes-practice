cd0b73edba7332e55dd860f285d9b05facd9ff2f
tugi-ngunjiri <blueqwinn@gmail.com>
2022-12-06 15:47:10 +0300

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+function  getpost(Create ,a, post)
+fetchhttp;//localhost:3000/likes
+
+function getquote(write ,a,quote)
+fetch;quoteId
+
+function getpost(Make,a,post)
+fetch;-postMessage(quoteId)
